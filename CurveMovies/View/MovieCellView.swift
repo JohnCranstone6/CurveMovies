63ac3f343ff973319954aa1dd32247ad67599acb
Johncranstone <johncranstone@hotmail.com>
2019-04-28 09:41:48 +0100

    Commit Test

---
+++ CurveMovies/View/MovieCellView.swift
@@ -0,0 +1,56 @@
+//
+//  Cell.swift
+//  CurveMovies
+//
+//  Created by John Cranstone on 25/04/2019.
+//  Copyright © 2019 John Cranstone. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class MovieCellView: UITableViewCell {
+ 
+    @IBOutlet weak var moviePoster: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieDescription: UILabel!
+    @IBOutlet weak var movieRatingLbl: UILabel!
+    @IBOutlet weak var releaseDate: UILabel!
+    @IBOutlet weak var likeImage: UIImageView!
+    
+    func setCell(count:Int){
+
+        
+        let url = URL(string: "http://image.tmdb.org/t/p/w500/"+moviePath[count])
+        
+        DispatchQueue.global().async {
+        
+            //download and present image using link above
+            
+        let data = try? Data(contentsOf: url!)
+            if let imageData = data {
+                let poster = UIImage(data: imageData)
+                    DispatchQueue.main.async {
+                    self.moviePoster.image = poster
+                    }
+            }
+        }
+        
+        //Set text from Movie model
+        movieTitle.text = movieName[count]
+        movieDescription.text = movieDesc[count]
+        releaseDate.text = movieRelease[count]
+        
+        
+        //Rating check
+        let formatReady = movieRating[count] * 10
+        if(formatReady < 40){
+            movieRatingLbl.textColor = UIColor.red
+        }else if(formatReady < 70 && formatReady >= 40 ){
+            movieRatingLbl.textColor = UIColor.orange
+        }else{
+            movieRatingLbl.textColor = UIColor.green
+        }
+        let ratingFormatted = String(format:"%.0f%%",formatReady)
+            movieRatingLbl.text = ratingFormatted
+    }
+}
